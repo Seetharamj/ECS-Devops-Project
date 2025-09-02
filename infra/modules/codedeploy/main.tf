a7f190115eb35078c694c349c492812e660c1e92
seetharam j <seetharamj704@gmail.com>
2025-09-02 14:44:44 +0530

    Update main.tf

--- infra/modules/codedeploy/main.tf
+++ infra/modules/codedeploy/main.tf
@@ -1,27 +1,106 @@
-resource "aws_codedeploy_app" "ecs" {
- name = "flask-app-codedeploy"
- compute_platform = "ECS"
+
+
+provider "aws" {
+  region = var.region
 }
+
+
+resource "aws_ecs_cluster" "this" {
+  name = var.ecs_cluster_name
+}
+
+resource "aws_ecs_task_definition" "app" {
+  family                   = "flask-app-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([{
+    name      = "flask-app"
+    image     = var.app_image
+    essential = true
+    portMappings = [{
+      containerPort = 5000
+      hostPort      = 5000
+    }]
+  }])
+}
+
+
+resource "aws_ecs_service" "app" {
+  name            = var.ecs_service_name
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = 2
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = var.public_subnet_ids
+    assign_public_ip = true
+    security_groups = [var.sg_id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.blue.arn
+    container_name   = "flask-app"
+    container_port   = 5000
+  }
+
+  deployment_controller {
+    type = "CODE_DEPLOY"
+  }
+
+  depends_on = [aws_lb_target_group.blue, aws_lb_target_group.green]
+}
+
+
+resource "aws_lb_target_group" "blue" {
+  name     = var.target_group_blue
+  port     = 5000
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  target_type = "ip"
+}
+
+resource "aws_lb_target_group" "green" {
+  name     = var.target_group_green
+  port     = 5000
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  target_type = "ip"
+}
+
+
+resource "aws_codedeploy_app" "ecs" {
+  name             = "flask-app-codedeploy"
+  compute_platform = "ECS"
+}
+
 resource "aws_codedeploy_deployment_group" "ecs" {
- app_name = aws_codedeploy_app.ecs.name
- deployment_group_name = "flask-app-deploy-group"
- service_role_arn = var.codedeploy_role_arn
- deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
- ecs_service {
- cluster_name = var.ecs_cluster_name
- service_name = var.ecs_service_name
- }
- load_balancer_info {
- target_group_pair_info {
- prod_traffic_route {
- listener_arns = [var.listener_arn]
- }
- target_group {
- name = var.target_group_blue
- }
- target_group {
- name = var.target_group_green
- }
- }
- }
+  app_name              = aws_codedeploy_app.ecs.name
+  deployment_group_name = "flask-app-deploy-group"
+  service_role_arn      = var.codedeploy_role_arn
+  deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
+
+  ecs_service {
+    cluster_name = aws_ecs_cluster.this.name
+    service_name = aws_ecs_service.app.name
+  }
+
+  load_balancer_info {
+    target_group_pair_info {
+      prod_traffic_route {
+        listener_arns = [var.listener_arn]
+      }
+
+      target_groups {
+        name = aws_lb_target_group.blue.name
+      }
+
+      target_groups {
+        name = aws_lb_target_group.green.name
+      }
+    }
+  }
 }
