10035c321a027a51085878fc4ed4a552b241a6cf
seetharam j <seetharamj704@gmail.com>
2025-09-02 15:48:34 +0530

    Update main.tf

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -6,45 +6,40 @@ resource "aws_ecs_cluster" "this" {
 }
 
 resource "aws_ecs_task_definition" "app" {
-  family                   = "flask-app-task"
+  family                   = "${var.name}-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
 
-  container_definitions = jsonencode([
-    {
-      name      = "flask-app"
-      image     = var.app_image
-      essential = true
-      portMappings = [
-        {
-          containerPort = 5000
-          hostPort      = 5000
-        }
-      ]
-    }
-  ])
+  container_definitions = jsonencode([{
+    name      = var.name
+    image     = var.app_image
+    essential = true
+    portMappings = [{
+      containerPort = var.container_port
+      hostPort      = var.container_port
+    }]
+  }])
 }
 
-
 resource "aws_ecs_service" "app" {
-  name            = "flask-app-service"
+  name            = "${var.name}-service"
   cluster         = aws_ecs_cluster.this.id
   task_definition = aws_ecs_task_definition.app.arn
   desired_count   = var.desired_count
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets          = var.subnets
+    subnets          = var.public_subnet_ids
     assign_public_ip = true
     security_groups  = [var.sg_id]
   }
 
   load_balancer {
     target_group_arn = var.target_group_arn
-    container_name   = "flask-app"
-    container_port   = 5000
+    container_name   = var.name
+    container_port   = var.container_port
   }
 
   deployment_controller {
