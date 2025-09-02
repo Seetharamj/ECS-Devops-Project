dad6c5a7e4e360968f060e3cfab3c2f1e1c733ed
seetharam j <seetharamj704@gmail.com>
2025-09-02 17:53:49 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -28,17 +28,16 @@ name = var.name
 }
 
 module "ecs" {
-  source            = "./modules/ecs"
-  cluster_name      = var.name
-  vpc_id            = module.networking.vpc_id
-  public_subnet_ids = module.networking.public_subnet_ids
-  container_port    = var.container_port
-  desired_count     = var.desired_count
-  app_health_check  = var.app_health_check
-  ecr_repo_url      = module.ecr.repository_url
-  sg_id             = var.sg_id
-  app_image         = var.app_image
-  target_group_arn   = module.alb.blue_target_group_arn 
+  source        = "./modules/ecs"
+  service_name  = var.name                       
+  vpc_id        = module.networking.vpc_id
+  subnets       = module.networking.public_subnet_ids
+  sg_id         = var.sg_id
+  container_port = var.container_port
+  desired_count = var.desired_count
+  app_image     = var.app_image
+  app_health_check = var.app_health_check
 }
+
 
 
