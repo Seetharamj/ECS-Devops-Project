03b14d7acd22d2102b5e9e263bf9e29af9896cd9
seetharam j <seetharamj704@gmail.com>
2025-09-02 15:33:45 +0530

    Update variables.tf

--- infra/modules/ecs/variables.tf
+++ infra/modules/ecs/variables.tf
@@ -1,51 +1,31 @@
-variable "region" {
-  description = "AWS Region"
-  type        = string
-  default     = "us-east-1"
+variable "name" {
+  type = string
 }
 
-variable "app_image" {
-  description = "Docker image for the Flask app"
-  type        = string
+variable "vpc_id" {
+  type = string
 }
 
-variable "subnets" {
-  description = "List of public subnet IDs"
-  type        = list(string)
+variable "public_subnet_ids" {
+  type = list(string)
 }
 
-variable "sg_id" {
-  description = "Security Group ID for ECS service"
-  type        = string
-}
-
-variable "target_group_arn" {
-  description = "ALB Target Group ARN"
-  type        = string
+variable "container_port" {
+  type = number
 }
 
 variable "desired_count" {
-  description = "Number of ECS tasks to run"
-  type        = number
-  default     = 2
-}
-
-variable "cluster_name" {
-  description = "ECS Cluster Name"
-  type        = string
-  default     = "devops-ecs-cluster"
+  type = number
 }
 
 variable "app_health_check" {
-  type = object({
-    path              = string
-    healthy_threshold = number
-    unhealthy_threshold = number
-    timeout           = number
-    interval          = number
-  })
+  type = string
 }
 
 variable "ecr_repo_url" {
+  type = string
+}
+
+variable "sg_id" {
   type = string
 }
