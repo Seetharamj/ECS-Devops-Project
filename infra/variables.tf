0015c91a668cda34c1c4376702037672a338f91f
seetharam j <seetharamj704@gmail.com>
2025-09-02 11:08:32 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,4 +1,8 @@
-variable "name" { type = string default = "adv-ecs-bluegreen" }
+variable "name" {
+  type    = string
+  default = "adv-ecs-bluegreen"
+}
+
 variable "region" { type = string default = "ap-south-1" }
 variable "vpc_cidr" { type = string default = "10.0.0.0/16" }
 variable "public_subnet_cidrs" { type = list(string) default = ["10.0.1.0/24", "10.0.2.0/24"] }
