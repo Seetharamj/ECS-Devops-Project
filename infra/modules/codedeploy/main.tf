a55b4669d46917a4e0a8567a61233b429046f2d3
seetharam j <seetharamj704@gmail.com>
2025-09-02 14:50:02 +0530

    Update main.tf

--- infra/modules/codedeploy/main.tf
+++ infra/modules/codedeploy/main.tf
@@ -1,14 +1,13 @@
-
-
 provider "aws" {
   region = var.region
 }
 
-
+# ECS Cluster
 resource "aws_ecs_cluster" "this" {
   name = var.ecs_cluster_name
 }
 
+# ECS Task Definition
 resource "aws_ecs_task_definition" "app" {
   family                   = "flask-app-task"
   network_mode             = "awsvpc"
@@ -27,7 +26,7 @@ resource "aws_ecs_task_definition" "app" {
   }])
 }
 
-
+# ECS Service
 resource "aws_ecs_service" "app" {
   name            = var.ecs_service_name
   cluster         = aws_ecs_cluster.this.id
@@ -50,37 +49,36 @@ resource "aws_ecs_service" "app" {
   deployment_controller {
     type = "CODE_DEPLOY"
   }
-
-  depends_on = [aws_lb_target_group.blue, aws_lb_target_group.green]
 }
 
-
+# ALB Target Groups
 resource "aws_lb_target_group" "blue" {
-  name     = var.target_group_blue
-  port     = 5000
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
+  name        = var.target_group_blue
+  port        = 5000
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
   target_type = "ip"
 }
 
 resource "aws_lb_target_group" "green" {
-  name     = var.target_group_green
-  port     = 5000
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
+  name        = var.target_group_green
+  port        = 5000
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
   target_type = "ip"
 }
 
-
+# CodeDeploy App
 resource "aws_codedeploy_app" "ecs" {
   name             = "flask-app-codedeploy"
   compute_platform = "ECS"
 }
 
+# CodeDeploy Deployment Group
 resource "aws_codedeploy_deployment_group" "ecs" {
-  app_name              = aws_codedeploy_app.ecs.name
-  deployment_group_name = "flask-app-deploy-group"
-  service_role_arn      = var.codedeploy_role_arn
+  app_name               = aws_codedeploy_app.ecs.name
+  deployment_group_name  = "flask-app-deploy-group"
+  service_role_arn       = var.codedeploy_role_arn
   deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
 
   ecs_service {
