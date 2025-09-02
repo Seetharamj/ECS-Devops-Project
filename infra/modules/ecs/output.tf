3b981ee11e30696ded20deb7b86004b910dd4408
seetharam j <seetharamj704@gmail.com>
2025-09-02 17:49:45 +0530

    Update output.tf

--- infra/modules/ecs/output.tf
+++ infra/modules/ecs/output.tf
@@ -5,15 +5,3 @@ output "cluster_name" {
 output "service_name" {
   value = aws_ecs_service.app.name
 }
-
-output "listener_arn" {
-  value = aws_lb_listener.app.arn
-}
-
-output "blue_target_group_arn" {
-  value = aws_lb_target_group.blue.arn
-}
-
-output "green_target_group_arn" {
-  value = aws_lb_target_group.green.arn
-}
