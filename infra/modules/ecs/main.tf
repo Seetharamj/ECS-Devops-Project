b7920ad9c8701c5ab515b51022ffba6451e5dc5b
seetharam j <seetharamj704@gmail.com>
2025-09-02 14:10:46 +0530

    Update main.tf

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -1,39 +1,51 @@
+
 resource "aws_ecs_cluster" "this" {
- name = "devops-ecs-cluster"
+  name = var.cluster_name
 }
+
 resource "aws_ecs_task_definition" "app" {
- family = "flask-app-task"
- network_mode = "awsvpc"
- requires_compatibilities = ["FARGATE"]
- cpu = "256"
- memory = "512"
- container_definitions = jsonencode([{
- name = "flask-app"
- image = var.app_image
- essential = true
- portMappings = [{
- containerPort = 5000
- hostPort = 5000
- }]
- }])
+  family                   = "flask-app-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name      = "flask-app"
+      image     = var.app_image
+      essential = true
+      portMappings = [
+        {
+          containerPort = 5000
+          hostPort      = 5000
+        }
+      ]
+    }
+  ])
 }
+
+
 resource "aws_ecs_service" "app" {
- name = "flask-app-service"
- cluster = aws_ecs_cluster.this.id
- task_definition = aws_ecs_task_definition.app.arn
- desired_count = 2
- launch_type = "FARGATE"
- network_configuration {
- subnets = var.subnets
- assign_public_ip = true
- security_groups = [var.sg_id]
- }
- load_balancer {
- target_group_arn = var.target_group_arn
- container_name = "flask-app"
- container_port = 5000
- }
- deployment_controller {
- type = "CODE_DEPLOY"
- }
+  name            = "flask-app-service"
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = var.desired_count
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = var.subnets
+    assign_public_ip = true
+    security_groups  = [var.sg_id]
+  }
+
+  load_balancer {
+    target_group_arn = var.target_group_arn
+    container_name   = "flask-app"
+    container_port   = 5000
+  }
+
+  deployment_controller {
+    type = "CODE_DEPLOY"
+  }
 }
