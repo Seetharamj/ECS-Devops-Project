d5de7c6156f12fc65a4c426119f08255b9166f7b
seetharam j <seetharamj704@gmail.com>
2025-09-02 19:17:57 +0530

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,4 +1,7 @@
-output "alb_dns" { value = module.ecs.alb_dns_name }
+output "alb_dns_name" {
+  value = aws_lb.app.dns_name
+}
+
 output "ecr_repository" { value = module.ecr.repository_url }
 output "service_name" { value = module.ecs.service_name }
 output "cluster_name" { value = module.ecs.cluster_name }
