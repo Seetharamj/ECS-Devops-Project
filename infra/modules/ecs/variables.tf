ace77d72e1f8cd441fad7f2bfbfe0dc85438e92e
seetharam j <seetharamj704@gmail.com>
2025-09-02 13:00:42 +0530

    Create variables.tf

---
+++ infra/modules/ecs/variables.tf
@@ -0,0 +1,33 @@
+variable "name" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "public_subnet_ids" {
+  type = list(string)
+}
+
+variable "container_port" {
+  type = number
+}
+
+variable "desired_count" {
+  type = number
+}
+
+variable "app_health_check" {
+  type = object({
+    path              = string
+    healthy_threshold = number
+    unhealthy_threshold = number
+    timeout           = number
+    interval          = number
+  })
+}
+
+variable "ecr_repo_url" {
+  type = string
+}
