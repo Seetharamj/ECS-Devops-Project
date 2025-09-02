e6f8fcc6a804060ef3507ded690423bc19171ba1
seetharam j <seetharamj704@gmail.com>
2025-09-02 18:28:57 +0530

    Update outputs.tf

--- infra/modules/alb/outputs.tf
+++ infra/modules/alb/outputs.tf
@@ -1,15 +1,3 @@
-output "alb_arn" {
-  value = aws_lb.app.arn
-}
-
-output "listener_arn" {
-  value = aws_lb_listener.app.arn
-}
-
-output "blue_target_group_arn" {
-  value = aws_lb_target_group.blue.arn
-}
-
-output "green_target_group_arn" {
-  value = aws_lb_target_group.green.arn
+output "alb_dns" {
+  value = module.alb.alb_dns_name
 }
